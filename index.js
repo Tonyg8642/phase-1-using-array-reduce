94151330664173fe1d02e2015f4c262685d5ea82
Tony Glass <idttip.tg@gmail.com>
2023-10-18 15:38:29 -0700

    Array Reduce Method

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce (myFunc);
+
+function myFunc(total, add) {
+    return total + add
+}
 
 // Code your solution here
